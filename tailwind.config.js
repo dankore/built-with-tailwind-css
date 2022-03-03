9989ab06ce50ec9ae4ad51c70842289ac0288667
Adamu Muhammad Dankore <adamu.muhammad@thegazette.com>
2022-03-03 06:39:51 -0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./src/popup.html', './src/popup.js'],
+  darkMode: 'media',
+  theme: {
+    fontFamily: {
+      display: ['Source Serif Pro', 'Georgia', 'serif'],
+      body: ['Synonym', 'system-ui', 'sans-serif'],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
